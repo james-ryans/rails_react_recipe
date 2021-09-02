28127035e4c8ace47646c594ab728c2226da4815
James Ryans <james.ryans2012@gmail.com>
2021-09-02 15:27:29 +0700

    add recipe model & controller

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      post 'recipes/create'
+      get 'recipes/show/:id', to: 'recipes#show'
+      delete 'recipes/destroy/:id', to: 'recipes#destroy'
+    end
+  end
+  root 'homepage#index'
+  get '/*path' => 'homepage#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
